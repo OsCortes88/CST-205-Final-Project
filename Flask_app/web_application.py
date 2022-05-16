ab5b3bca785bb12982c5f60fa9c054634f752f24
OsCortes88 <ocortestinoco@csumb.edu>
2022-05-16 01:14:20 -0700

    Scale Up Edits

--- Flask_app/web_application.py
+++ Flask_app/web_application.py
@@ -1,21 +1,24 @@
 from flask import Flask, render_template, redirect, request
 from flask_wtf import FlaskForm
-from wtforms import FileField, SubmitField
+from wtforms import FileField, SubmitField, IntegerField, SelectField, validators
 from flask_bootstrap import Bootstrap5
 from werkzeug.utils import secure_filename
 import os
 from wtforms.validators import InputRequired
 from PIL import Image
+import numpy as np
 
 # Starts Flask application
 app = Flask(__name__)
 # Class for multimedia filters.
 class ImageFilter():
-    def __init__(self, file_data, filter):
+    def __init__(self, file_data, filter, scale, direction):
         # super().__init__()
         self.file_name = file_data.filename
         self.image = Image.open(file_data)
         self.filter = filter
+        self.scale = scale
+        self.direction = direction
         self.applyfilter()
 
     def applyfilter(self):
@@ -26,7 +29,7 @@ class ImageFilter():
         elif self.filter == 'sepia':
             self.sepia_filter()
         elif self.filter == 'scale':
-            self.thumbnail_filter(5)
+            self.thumbnail_filter(self.scale, self.direction)
 
     def grayscale_filter(self):
         # Grayscale by illuminance method.
@@ -42,26 +45,41 @@ class ImageFilter():
         self.image.putdata(list(self.img_list))
         self.image.save(f"static/files/{self.file_name}")
 
-    def thumbnail_filter(self, scale):
-        # Thumbnail
-        self.w, self.h = self.image.width//scale, self.image.height//scale
-        self.my_trgt = Image.new('RGB', (self.w, self.h))
+    def thumbnail_filter(self, scale, direction):
+        if direction == "Reduce Image":
+            # Scale down
+            self.w, self.h = self.image.width//scale, self.image.height//scale
+            self.my_trgt = Image.new('RGB', (self.w, self.h))
 
-        self.target_x = 0
-        for source_x in range(0, self.image.width, scale):
-            self.target_y = 0
-            for source_y in range(0, self.image.height, scale):
-                self.p = self.image.getpixel((source_x, source_y))
-                try:
-                    self.my_trgt.putpixel((self.target_x,self.target_y), self.p)
-                except:
-                    break
-                self.target_y += 1
-            self.target_x += 1
-            
-        self.my_trgt.save(f"static/files/{self.file_name}")
+            self.target_x = 0
+            for source_x in range(0, self.image.width, scale):
+                self.target_y = 0
+                for source_y in range(0, self.image.height, scale):
+                    self.p = self.image.getpixel((source_x, source_y))
+                    try:
+                        self.my_trgt.putpixel((self.target_x,self.target_y), self.p)
+                    except:
+                        break
+                    self.target_y += 1
+                self.target_x += 1
+                
+            self.my_trgt.save(f"static/files/{self.file_name}")
+        else:
+            # scale up
+            w, h = self.image.width*scale, self.image.height*scale
 
-        # Implement a scale up image function.
+            self.my_trgt = Image.new('RGB', (w, h))
+            target_x = 0
+            for source_x in np.repeat(range(self.image.width), scale):
+                target_y = 0
+                for source_y in np.repeat(range(self.image.height), scale):
+                    p = self.image.getpixel((int(source_x), int(source_y)))
+                    self.my_trgt.putpixel((target_x, target_y), p)
+                    target_y += 1
+                target_x += 1
+
+            self.my_trgt.save(f"static/files/{self.file_name}")
+
 
     def sepia(self, p):
         self.p = p
@@ -100,6 +118,12 @@ bootstrap = Bootstrap5(app)
 ALLOWED_EXTENSIONS = set([".jpg", ".png", ".PNG"])
 
 # Class for the file submission form
+class ImageUploadForScale(FlaskForm):
+    image_file = FileField("File", [validators.DataRequired()])
+    submit = SubmitField("Upload File")
+    scale = IntegerField("Scale Factor", [validators.DataRequired(), validators.NumberRange(2, 10)])
+    up_or_down = SelectField(u'Scale Choice', choices = ['Reduce Image', 'Enlarge Image'])
+
 class ImageUpload(FlaskForm):
     image_file = FileField("File", validators=[InputRequired()])
     submit = SubmitField("Upload File")
@@ -112,12 +136,22 @@ def run_home():
 # This is the upload page that is used to sumbit the image
 @app.route("/upload-image/<filter>", methods=["GET", "POST"])
 def upload_image(filter):
-    form = ImageUpload()
-    if form.validate_on_submit():
+    if filter == 'scale':
+        form = ImageUploadForScale()
+        if form.validate_on_submit():
 
-        if  form.image_file.data.filename[form.image_file.data.filename.find('.'):] in ALLOWED_EXTENSIONS:
-            file = form.image_file.data # First grab the file
-            test = ImageFilter(file, filter)
-            return render_template("website_code.html", form=form, ran_filter = 'True')
-    return render_template("website_code.html", form=form, ran_filter = 'False')
+            if  form.image_file.data.filename[form.image_file.data.filename.find('.'):] in ALLOWED_EXTENSIONS:
+                file = form.image_file.data # First grab the file
+                test = ImageFilter(file, filter, form.scale.data, form.up_or_down.data)
+            return render_template("website_code.html", form=form, filter = filter,  ran_filter = 'True')
+    else:
+        form = ImageUpload()
+        if form.validate_on_submit():
+
+            if  form.image_file.data.filename[form.image_file.data.filename.find('.'):] in ALLOWED_EXTENSIONS:
+                file = form.image_file.data # First grab the file
+                test = ImageFilter(file, filter, 1, "")
+            return render_template("website_code.html", form=form, filter = filter, ran_filter = 'True')
+
+    return render_template("website_code.html", form=form, filter = filter, ran_filter = 'False')
 
